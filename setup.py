f267ce6fff056d28960426aaf03723b483bf0545
Andrew Tedstone <a.j.tedstone@bristol.ac.uk>
2016-12-22 15:02:40 +0000

    remove requires_

--- setup.py
+++ setup.py
@@ -2,14 +2,13 @@ from distutils.core import setup
 setup(
   name = 'georaster',
   packages = ['georaster'], # this must be the same as the name above
-  version = '1.14',
+  version = '1.15',
   description = 'easy processing and analysis of geographic and projected rasters in Python',
   author = 'Andrew Tedstone, Amaury Dehecq',
   author_email = 'a.j.tedstone@bristol.ac.uk',
   url = 'https://github.com/atedstone/georaster', # use the URL to the github repo
-  download_url = 'https://github.com/atedstone/georaster/archive/1.14.tar.gz',
+  download_url = 'https://github.com/atedstone/georaster/archive/1.15.tar.gz',
   keywords = ['rasters', 'remote sensing', 'GIS', 'GDAL', 'projections', 'geoTIFF'], # arbitrary keywords
   classifiers = [],
-  license = 'GNU GPL v.3',
-  install_requires = ['numpy', 'scipy', 'gdal', 'pyproj']
+  license = 'GNU GPL v.3'
 )
