fa17e4047ea987233d13e48475c6c2e7d5fcee4d
Andrew Tedstone <a.j.tedstone@bristol.ac.uk>
2016-12-22 10:48:44 +0000

    packaging changes

--- setup.py
+++ setup.py
@@ -2,12 +2,12 @@ from distutils.core import setup
 setup(
   name = 'georaster',
   packages = ['georaster'], # this must be the same as the name above
-  version = '1.1',
+  version = '1.11',
   description = 'easy processing and analysis of geographic and projected rasters in Python',
   author = 'Andrew Tedstone, Amaury Dehecq',
   author_email = 'a.j.tedstone@bristol.ac.uk',
-  url = 'https://github.com/ajtedstone/georaster', # use the URL to the github repo
-  download_url = 'https://github.com/ajtedstone/georaster/tarball/1.1', # I'll explain this in a second
+  url = 'https://github.com/atedstone/georaster', # use the URL to the github repo
+  download_url = 'https://github.com/atedstone/georaster/tarball/1.11',
   keywords = ['rasters', 'remote sensing', 'GIS', 'GDAL', 'projections', 'geotiff'], # arbitrary keywords
   classifiers = [],
   license = 'GNU GPL v.3'
