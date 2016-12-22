65814b0cdd4b06cfb2df1f29fbed99f9f8449baf
Andrew Tedstone <a.j.tedstone@bristol.ac.uk>
2016-12-22 11:46:45 +0000

    fix gdal packaging

--- setup.py
+++ setup.py
@@ -2,13 +2,14 @@ from distutils.core import setup
 setup(
   name = 'georaster',
   packages = ['georaster'], # this must be the same as the name above
-  version = '1.11',
+  version = '1.13',
   description = 'easy processing and analysis of geographic and projected rasters in Python',
   author = 'Andrew Tedstone, Amaury Dehecq',
   author_email = 'a.j.tedstone@bristol.ac.uk',
   url = 'https://github.com/atedstone/georaster', # use the URL to the github repo
-  download_url = 'https://github.com/atedstone/georaster/tarball/1.11',
+  download_url = 'https://github.com/atedstone/georaster/tarball/1.13',
   keywords = ['rasters', 'remote sensing', 'GIS', 'GDAL', 'projections', 'geotiff'], # arbitrary keywords
   classifiers = [],
-  license = 'GNU GPL v.3'
+  license = 'GNU GPL v.3',
+  install_requires = ['numpy', 'scipy', 'gdal', 'pyproj', 'warnings']
 )
